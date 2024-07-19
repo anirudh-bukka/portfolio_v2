c41867777d2703a83e172ce64f0dfe08f5f117a5
anirudh-bukka <anirudh.bukka@gmail.com>
2024-07-19 11:21:45 +0530

    Completed home page.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 // import "react-vertical-timeline-component/style.min.css";
+import room1 from '../assets/pictures/room1.jpg';
+import manali from '../assets/pictures/manali1.jpg';
+import shimla from '../assets/pictures/shimla1.jpg';
+import bonfire from '../assets/pictures/bonfire1.jpg';
+import diwali from '../assets/pictures/diwali1.jpeg';
+import speaker from '../assets/pictures/speaker1.jpg';
+import stadium from '../assets/pictures/cricket-stadium1.jpg';
+import cycletrack1 from '../assets/pictures/cycletrack1.jpg';
+import cycletrack2 from '../assets/pictures/cycletrack2.jpg';
+import warangalgate from '../assets/pictures/warangalgate1.jpg';
 
 function HomePage() {
   return (
-    <div className='sm:p-1 mt-2 flex flex-col w-full'>
-        Hello
+    <div className='mt-2 flex flex-col max-sm:w-[90%] lg:w-full mx-auto'>
+        <p className='lg:text-6xl lg:ml-64 lg:py-8 lg:w-[50%] max-sm:text-3xl'>Hi, I'm a software engineer and have a passion for music, basketball and photography.</p>
+        <p className='lg:text-3xl lg:ml-64 mb-8 pl-1'><i>Inspired by big dreams and dank memes.</i></p>
+        <div className='flex flex-col justify-evenly'>
+            <img src={room1} className='py-2' alt="Room" />
+            <div className='flex lg:flex-row max-sm:flex-col'>
+                <img src={manali} className='lg:w-[50%] lg:h-[50%] lg:pr-1 py-2' alt="Manali" />
+                <img src={shimla} className='lg:w-[50%] lg:h-[50%] lg:pl-1 py-2' alt="Shimla" />
+            </div>
+            <div className='flex lg:flex-row max-sm:flex-col justify-evenly'>
+                <img src={cycletrack1} className='lg:w-[33%] lg:h-[33%] lg:pr-1 py-2' alt="Cycling Track" />
+                <img src={cycletrack2} className='lg:w-[33%] lg:h-[33%] py-2' alt="Cycling Track" />
+                <img src={warangalgate} className='lg:w-[33%] lg:h-[33%] lg:pl-1 py-2' alt="Warangal Gate Mock" />
+            </div>
+            <div className='flex lg:flex-row max-sm:flex-col'>
+                <img src={bonfire} className='lg:w-[50%] lg:h-[50%] lg:pr-1 py-2' alt="Bonfire" />
+                <img src={speaker} className='lg:w-[50%] lg:h-[920px] lg:pr-1 py-2' alt="Me speaking" />
+                {/* <img src={diwali} className='lg:w-[50%] lg:h-[920px] lg:pl-1 py-2' alt="Flowerpot" /> */}
+            </div>
+            <img src={stadium} className='py-2' alt="Cricket Stadium" />
+        </div>
     </div>
   )
 }
